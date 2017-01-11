c5e89e8e45f76888e4f45127542a2e8a505cca7f
Justin Young <jyoung@sonatype.com>
2017-01-10 16:06:01 -0800

    Support for training model in application.

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ import cv2
 import gobject
 import gtk
 import numpy
+import pango
 
 import face_recognizer
 import settings
@@ -17,36 +18,27 @@ class MillieMirror:
     def __init__(self):
         self.settings_marshaller = settings.SettingsMarshaller()
         self.settings = self.settings_marshaller.settings
-        self.face_recognizer = face_recognizer.FaceRecognizer()
+        self.face_recognizer = face_recognizer.FaceRecognizer(self.settings)
 
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.connect("destroy", gtk.main_quit)
-        self.window.connect("key_press_event", self.on_key_press_event)
-        self.window.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(0, 0, 0))
+        self.gtk_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.gtk_window.connect("destroy", gtk.main_quit)
+        self.gtk_window.connect("key_press_event", self.on_key_press_event)
+        self.gtk_window.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(0, 0, 0))
 
         self.gtk_fixed = gtk.Fixed()
 
-        self.info_text = gtk.TextView()
-        self.info_text.set_editable(False)
-        self.info_text.set_cursor_visible(False)
-        self.text_buffer = gtk.TextBuffer()
-        self.info_text.set_buffer(self.text_buffer)
-        self.event_box_info_text = gtk.EventBox()
-        self.event_box_info_text.add(self.info_text)
-        self.gtk_fixed.add(self.event_box_info_text)
-
-        self.window.add(self.gtk_fixed)
-        self.window.fullscreen()
+        self.gtk_window.add(self.gtk_fixed)
+        self.gtk_window.fullscreen()
         self.gtk_fixed.show()
-        self.window.show()
-        self.screen_width, self.screen_height = self.window.get_size()
+        self.gtk_window.show()
+        self.screen_width, self.screen_height = self.gtk_window.get_size()
 
         self.gtk_drawing_area = gtk.DrawingArea()
         self.gtk_drawing_area.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(0, 0, 0))
         self.gtk_drawing_area.connect("expose_event", self.expose_event)
         self.gtk_drawing_area.connect("configure_event", self.configure_event)
         self.gtk_drawing_area.set_size_request(self.screen_width, self.screen_height)
-        self.pixmap = None
+        self.gtk_pixmap = None
         self.gtk_fixed.add(self.gtk_drawing_area)
         self.gtk_drawing_area.show()
 
@@ -59,13 +51,15 @@ class MillieMirror:
         self.settings_marshaller.default_if_none('recognition_threshold', 80)
         self.settings_marshaller.save()
 
-        self.face_recognizer.update_threshold(self.settings.recognition_threshold)
-
         self.camera = webcam.OpenCVCapture(device_id=0)
 
         self.background_thread = threading.Thread(target=self.background_work)
         self.background_thread.daemon = True
         self.kill_background = False
+
+        self.recognizing_face = False
+        self.temporary_notice_timer = None
+        self.temporary_notice = None
 
     def main(self):
         self.background_thread.start()
@@ -90,11 +84,9 @@ class MillieMirror:
             background_image = cv2.resize(background_image,
                                           (self.settings.background_image_width, self.settings.background_image_height))
 
-
             # image_cv = cv2.cv.fromarray(background_image)
             #
             # image_final = numpy.asarray(image_cv)
-
 
             background_image = background_image[..., ::-1]
 
@@ -128,12 +120,18 @@ class MillieMirror:
             self.settings.left_pad += 10
         elif event.keyval == gtk.keysyms.greater:
             self.settings.recognition_threshold += 20
-            self.face_recognizer.update_threshold(self.settings.recognition_threshold)
+            self.set_temporary_notice(
+                "Threshold: " + str(self.settings.recognition_threshold) + ". Restart to take affect.")
         elif event.keyval == gtk.keysyms.less:
             self.settings.recognition_threshold -= 20
-            self.face_recognizer.update_threshold(self.settings.recognition_threshold)
+            self.set_temporary_notice(
+                    "Threshold: " + str(self.settings.recognition_threshold) + ". Restart to take affect.")
         elif event.keyval == gtk.keysyms.f:
             self.settings.showing_face_rectangles = not self.settings.showing_face_rectangles
+        elif event.keyval == gtk.keysyms.a:
+            self.on_a_press()
+        elif event.keyval == gtk.keysyms.Return:
+            self.on_enter_press()
 
         self.settings_marshaller.save()
 
@@ -141,38 +139,91 @@ class MillieMirror:
         return int(self.screen_width / 100), int(self.screen_height / 100)
 
     def foreground_work(self, transform, background_image, faces):
-        self.pixmap.draw_rectangle(self.window.get_style().black_gc, gtk.TRUE, 0, 0, self.screen_width,
-                                   self.screen_height)
+        self.gtk_pixmap.draw_rectangle(self.gtk_window.get_style().black_gc, gtk.TRUE, 0, 0, self.screen_width,
+                                       self.screen_height)
 
         self.draw_background_image(transform, background_image)
         self.draw_face_rectangles(transform, faces)
+        self.draw_help_text()
 
         self.gtk_drawing_area.queue_draw_area(0, 0, self.screen_width, self.screen_height)
 
     def configure_event(self, widget, event):
         x, y, width, height = widget.get_allocation()
-        self.pixmap = gtk.gdk.Pixmap(widget.window, width, height)
+        self.gtk_pixmap = gtk.gdk.Pixmap(widget.window, width, height)
 
     def expose_event(self, widget, event):
-        if self.pixmap is not None:
+        if self.gtk_pixmap is not None:
             x, y, width, height = event.area
             graphics_context = widget.window.new_gc()
-            widget.window.draw_drawable(graphics_context, self.pixmap, x, y, x, y, width, height)
+            widget.window.draw_drawable(graphics_context, self.gtk_pixmap, x, y, x, y, width, height)
 
     def draw_background_image(self, transform, background_image):
         if self.settings.showing_background_image:
             x, y, scale_width, scale_height = transform
             img_pixbuf = gtk.gdk.pixbuf_new_from_array(background_image, gtk.gdk.COLORSPACE_RGB, 8)
             graphics_context = self.gtk_drawing_area.window.new_gc()
-            self.pixmap.draw_pixbuf(graphics_context, img_pixbuf, 0, 0, x, y, -1, -1, gtk.gdk.RGB_DITHER_NONE, 0, 0)
+            self.gtk_pixmap.draw_pixbuf(graphics_context, img_pixbuf, 0, 0, x, y, -1, -1, gtk.gdk.RGB_DITHER_NONE, 0, 0)
 
     def draw_face_rectangles(self, transform, faces):
         if self.settings.showing_face_rectangles:
             x, y, scale_width, scale_height = transform
+            white_gc = self.gtk_window.get_style().white_gc
             for face in faces:
-                self.pixmap.draw_rectangle(self.window.get_style().white_gc, gtk.FALSE, int(face.x * scale_width) + x,
-                                           int(face.y * scale_height) + y, int(face.w * scale_width),
-                                           int(face.h * scale_height))
+                self.gtk_pixmap.draw_rectangle(white_gc, gtk.FALSE, int(face.x * scale_width) + x,
+                                               int(face.y * scale_height) + y, int(face.w * scale_width),
+                                               int(face.h * scale_height))
+                pango_layout = self.gtk_drawing_area.create_pango_layout(face.user)
+                font_description = pango.FontDescription("sans bold 36")
+                pango_layout.set_font_description(font_description)
+                text_pad = 2
+                self.gtk_pixmap.draw_layout(white_gc, int(face.x * scale_width) + x + text_pad,
+                                            int(face.y * scale_height) + y + text_pad, pango_layout)
+
+    def draw_help_text(self):
+        text = ""
+
+        if self.recognizing_face:
+            text = "Recognizing Face: Press Enter to capture. Press a to exit mode and train."
+
+        if self.temporary_notice is not None:
+            text += "\n" + self.temporary_notice
+
+        white_gc = self.gtk_window.get_style().white_gc
+        pango_layout = self.gtk_drawing_area.create_pango_layout(text)
+        font_description = pango.FontDescription("sans bold 36")
+        pango_layout.set_font_description(font_description)
+        text_pad = 2
+        self.gtk_pixmap.draw_layout(white_gc, text_pad, text_pad, pango_layout)
+
+    def on_a_press(self):
+        if not self.recognizing_face:
+            self.recognizing_face = True
+            self.face_recognizer.init_new_face()
+        else:
+            self.recognizing_face = False
+            self.face_recognizer.train_faces()
+
+    def on_enter_press(self):
+        if self.recognizing_face:
+            image = numpy.copy(self.camera.read())
+            image = cv2.flip(image, 1)
+            success, error = self.face_recognizer.add_new_face(image)
+            if not success:
+                self.set_temporary_notice(error)
+
+    def set_temporary_notice(self, text):
+        if self.temporary_notice_timer is not None:
+            self.temporary_notice_timer.cancel()
+
+        self.temporary_notice = text
+
+        def reset_notice():
+            self.temporary_notice = None
+            self.temporary_notice_timer = None
+
+        self.temporary_notice_timer = threading.Timer(2.0, reset_notice)
+        self.temporary_notice_timer.start()
 
 
 if __name__ == "__main__":
--- face_recognizer.py
+++ face_recognizer.py
@@ -1,37 +1,95 @@
+import os
+
 import cv2
 import numpy
-import os
 
 
 class FaceRecognizer:
-    def __init__(self):
+    def __init__(self, settings):
         self.training_file_name = 'training.xml'
-        self.cascade_file_name = 'haarcascade_frontalface_alt'
+        self.cascade_file_name = 'haarcascade_frontalface_alt.xml'
+        self.training_image_directory = 'training_images'
+        self.training_user = None
+        self.training_user_iteration = None
+        self.settings = settings
 
         self.model = None
         if os.path.isfile(self.training_file_name):
-            self.model = cv2.createLBPHFaceRecognizer()
+            self.model = cv2.createLBPHFaceRecognizer(threshold=self.settings.recognition_threshold)
             self.model.load(self.training_file_name)
 
         self.cascade_classifier = cv2.CascadeClassifier(self.cascade_file_name)
 
-    def update_threshold(self, threshold):
-        if os.path.isfile(self.training_file_name):
-            self.model = cv2.createLBPHFaceRecognizer(threshold)
-            self.model.load(self.training_file_name)
-
     def detect_faces(self, image):
         image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        faces = self.cascade_classifier.detectMultiScale(image, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
-                                                        flags=cv2.CASCADE_SCALE_IMAGE)
+
+        faces = self.cascade_classifier.detectMultiScale(image, scaleFactor=1.2, minNeighbors=3, minSize=(50, 50),
+                                                        flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
 
         detected_faces = []
         for face in faces:
             x, y, w, h = face
             image_face = numpy.copy(image)
             image_face = image_face[y:y + h, x:x + w]
-            label, confidence = self.model.predict(image_face)
-            detected_faces.append(type('obj', (object,), {'user': label, 'x': x, 'y': y, 'w': w, 'h': h}))
+
+            label = -1
+            if self.model is not None:
+                try:
+                    label, confidence = self.model.predict(image_face)
+                except:
+                    label = -1
+
+            detected_faces.append(type('obj', (object,), {'user': str(label), 'x': x, 'y': y, 'w': w, 'h': h}))
 
         return detected_faces
 
+    def init_new_face(self):
+        if not os.path.exists(self.training_image_directory):
+            os.makedirs(self.training_image_directory)
+
+        i = 0
+        while True:
+            i += 1
+            potential_directory = os.path.join(self.training_image_directory, '%03d' % i)
+            if not os.path.exists(potential_directory):
+                os.makedirs(potential_directory)
+                self.training_user = i
+                self.training_user_iteration = 0
+                break
+
+    def add_new_face(self, image):
+        if self.training_user is None or self.training_user_iteration is None:
+            return False, "Face training was not initiated."
+
+        faces = self.detect_faces(image)
+        if len(faces) == 0:
+            return False, "No face detected."
+        elif len(faces) > 1:
+            return False, "More than one face detected."
+
+        face = faces[0]
+        cropped = image[face.y:face.y + face.h, face.x:face.x + face.w]
+        filename = os.path.join(self.training_image_directory, '%03d' % self.training_user,
+                                '%03d.pgm' % self.training_user_iteration)
+        cv2.imwrite(filename, cropped)
+        self.training_user_iteration += 1
+        return True, ""
+
+    def train_faces(self):
+        self.training_user = None
+        self.training_user_iteration = None
+
+        training_model = cv2.createLBPHFaceRecognizer()
+        images = []
+        labels = []
+        for directory in os.listdir(self.training_image_directory):
+            joined_directory = os.path.join(self.training_image_directory, directory)
+            if os.path.isdir(joined_directory):
+                for image_file in os.listdir(joined_directory):
+                    joined_file = os.path.join(joined_directory, image_file)
+                    image = cv2.imread(joined_file, cv2.IMREAD_GRAYSCALE)
+                    images.append(image)
+                    labels.append(int(directory))
+        training_model.train(numpy.asarray(images), numpy.asarray(labels))
+        training_model.save(self.training_file_name)
+        self.model = training_model
